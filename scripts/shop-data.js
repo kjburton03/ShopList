e286453b2f4e90ea59a4170e35133f28ac6b063e
kimberly burton <kj.burton03@gmail.com>
2026-04-08 12:00:35 -0400

    added data

---
+++ scripts/shop-data.js
@@ -0,0 +1,12 @@
+export const items = [
+    {
+        title: "murphys bed",
+        image: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSesvslqbk-UVd2uqJH06nzZ88mMInH6gWV7Mt4C2fRW6_W2xpEB-pBpSQPYd_0Uh4iOfeL5Za0xbLzi-CNydrWIx5yxAlgwFC4QkRk3SA",
+        price: 599.99
+    },
+    {
+        title: "leather sectional",
+        image: "https://i5.walmartimages.com/asr/5f082d86-531a-4b80-a68e-1f3c603c8d9d.f7968ccd9bfbd96d7c4c30d8bf23311f.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF",
+        price: 899.99
+    }
+]
